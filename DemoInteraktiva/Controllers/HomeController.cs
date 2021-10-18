b9109758efbe64ea95c00050f58ccfc5caf1d799
Erik Öberg <erik.oberg@miun.se>
2021-10-18 17:09:20 +0200

    Add project files.

---
+++ DemoInteraktiva/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DemoInteraktiva.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoInteraktiva.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // smart kod som hämtar från ett api
+            // hantera resultatet
+            return View();
+        }
+
+    }
+}
